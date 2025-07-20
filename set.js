1cc46ef1e080f409e429b4ede98faeb90b4cbc3a
Arthur324871 <arthurdanjacobfrancois@gmail.com>
2025-07-20 14:21:52 -0400

    Update set.js

--- set.js
+++ set.js
@@ -6,7 +6,7 @@ module.exports = {
   NUMERO_OWNER: process.env.NUMERO_OWNER || "",
   MODE: process.env.MODE || "public",
   MENU: process.env.MENU || "https://i.ibb.co/ynx9QcZ/image.jpg",
-  SESSION_ID: process.env.SESSION_ID || "ovl",
+  SESSION_ID: process.env.SESSION_ID || "Ovl-MD_HXzErKCy_SESSION-ID",
   LEVEL_UP: process.env.LEVEL_UP || "non",
   STICKER_PACK_NAME: process.env.STICKER_PACK_NAME || "Wa-sticker",
   STICKER_AUTHOR_NAME: process.env.STICKER_AUTHOR_NAME || "OVL-MD",
